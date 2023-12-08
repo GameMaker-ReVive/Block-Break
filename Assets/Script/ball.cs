6c9e9e03b9d31bad2d2805b58d651a68d7425c51
sun88110 <138852390+sun88110@users.noreply.github.com>
2023-12-08 16:53:12 +0900

    2023_12_08 last test

--- Assets/Script/ball.cs
+++ Assets/Script/ball.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class ball : MonoBehaviour
 {
-    public float initialForce = 10f;
-    private Rigidbody rb;
+    public Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        LaunchBall();
+        PushStart();
+    }
+    private void FixedUpdate()
+    {
+        
     }
 
-    void LaunchBall()
+/*    void LaunchBall()
     {
         // 랜덤한 방향으로 초기 힘을 가해서 공을 발사
         Vector3 initialDirection = new Vector3(Random.Range(-1f, 1f), 1f, Random.Range(-1f, 1f)).normalized;
-        rb.AddForce(initialDirection * initialForce, ForceMode.Impulse);
+        rb.AddForce(initialDirection * 10f, ForceMode.Impulse);
+    }*/
+    
+    void PushStart()
+    {
+        rb.AddForce(Vector3.down * 15f, ForceMode.Impulse);
     }
 
 }
--- Assets/Script/block.cs
+++ Assets/Script/block.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class block : MonoBehaviour
 {
+    public Material[] HP;
+    int i;
+    private void Start()
+    {
+        int ran = Random.Range(0, HP.Length);
+        gameObject.GetComponent<Renderer>().material = HP[ran];
+        i = ran;
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.tag == "ball")
         {
-            Destroy(gameObject);
+            if (i == 0)
+            {
+                Destroy(gameObject);
+            }
+            else
+                {
+                    gameObject.GetComponent<Renderer>().material = HP[i - 1];
+                i--;
+                }
+            }
         }
-    }
 }
